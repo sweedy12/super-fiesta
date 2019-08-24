97b0169bc84b45d3c80a7f857bef416c95a9f667
bgalb <gal.katzhendler@mail.huji.ac.il>
2019-08-24 16:19:08 +0300

    hi

--- run_k_means_pca.m
+++ run_k_means_pca.m
@@ -8,23 +8,7 @@ function [B,Z] = run_k_means_pca(X,k,d)
 
 %performing K-means:
 Z = kmeans(X,k);
-B = cell(1,k);
-Xs = size(X);
-p = Xs(2);
-%going through all clusters, performing PCA:
-for i=1:k
-    %creating array to hold the current base:
-    cur_b = zeros(p,d);
-    clu_ind = find(Z==i);
-    clu_points = X(clu_ind,:);
-    coeff = pca(clu_points);
-    %making sure the base would be padded (when n < p)
-    cs = size(coeff);
-    cmin  = min(cs(2),d);
-    %taking the leading d components 
-    cur_b(:,1:cmin)= coeff(:,1:cmin);
-    B{i}=  cur_b;
-end
+B = bases_for_clusters(X,Z,k,d)
 Z = Z'; %transposing Z to match previous dimension calls.
 
 end
